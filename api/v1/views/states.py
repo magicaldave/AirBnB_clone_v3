80079b151d7abbbd5a1a13518a92fa12a8dc7f56
magicaldave <smokydave@icloud.com>
2022-12-01 01:01:05 -0600

    Edit: added state_id_get and state_id_delete methods

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -20,3 +20,32 @@ def state_get():
     for state in all_states.values():
         states_list.append(state.to_dict())
     return make_response(jsonify(states_list))
+
+
+@app_views.route("/states/<state_id>", methods=["GET"],
+                 strict_slashes=False)
+def state_id_get(state_id):
+    """
+    Retrieves a specified state
+    Or 404 if not matched.
+    """
+    state = storage.get(State, state_id)
+    if state is None:
+        abort(404)
+    return make_response(jsonify(state.to_dict()))
+
+
+@app_views.route("/states/<state_id>", methods=["DELETE"],
+                 strict_slashes=False)
+def state_id_delete(state_id):
+    """
+    Deletes a specific state ID.
+    Raise 404 if not matched.
+    Otherwise, return an empty dict with status 200
+    """
+    state = storage.get(State, state_id)
+    if state is None:
+        abort(404)
+    storage.delete(state)
+    storage.save()
+    return make_response(jsonify({}), 200)
